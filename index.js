fdfc8349ca1c989e88a3f7fd46b01e7083ba3d05
mher <mmelqonyan1990@gmail.com>
2018-12-17 22:06:24 +0400

    beautify

--- index.js
+++ index.js
@@ -4,76 +4,76 @@ var MongoClient = require('mongodb').MongoClient
 var app = express();
 var ObjectID = require('mongodb').ObjectID
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 var db;
-app.get('/', (req, res)=>{//skzbnakan ejum grum e "hello"
+app.get('/', (req, res) => {//skzbnakan ejum grum e "hello"
     res.send('Hello');
 })
-app.get('/artists', (req, res)=>{//"/artist" ejum grum e artArr json@
-db.collection('artists').find().toArray((err, docs)=>{
-    if(err){
-        console.log(err);
-       return res.sendStatus(500);
+app.get('/artists', (req, res) => {//"/artist" ejum grum e artArr json@
+    db.collection('artists').find().toArray((err, docs) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
 
-    }
-    res.send(docs)
+        }
+        res.send(docs)
+    })
 })
+app.get('/artists/:id', (req, res) => {//"artists/id" hmp id ov grum e hmp ejum
+    db.collection('artists').findOne({ _id: ObjectID(req.params.id) }, (err, doc) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(doc.name)
+    })
 })
-app.get('/artists/:id', (req,res)=>{//"artists/id" hmp id ov grum e hmp ejum
-db.collection('artists').findOne({_id: ObjectID(req.params.id)}, (err, doc)=>{
-    if(err){
-        console.log(err);
-        return res.sendStatus(500);
-    }
-    res.send(doc.name)
-})
-})
-app.post('/artists', (req, res)=>{//req bodyn vercnum e sarqum object u grum /artistsum
-   var artist = {
-       name: req.body.name
-   };
+app.post('/artists', (req, res) => {//req bodyn vercnum e sarqum object u grum /artistsum
+    var artist = {
+        name: req.body.name
+    };
 
     console.log(req.body);
-   db.collection('artists').insert(artist, function(err, result){
-       if(err){
+    db.collection('artists').insert(artist, function (err, result) {
+        if (err) {
             console.log(err);
-          return  res.sendStatus(500);
-       }
-       res.send(artist);
-   })
+            return res.sendStatus(500);
+        }
+        res.send(artist);
+    })
 })
 
-app.put('/artists/:id', (req, res)=>{ //poxum e hmp id i tak exac name mer req ov
+app.put('/artists/:id', (req, res) => { //poxum e hmp id i tak exac name mer req ov
     db.collection('artists').updateOne(
-      { _id: ObjectID(req.params.id) },
-     {$set: {name: req.body.name}},
-       (err, result)=>{
-          if(err){
-              console.log("123")
-              console.log(err);
-              return res.sendStatus(500)
-          }
-          res.sendStatus(200)
-      } 
-      )
+        { _id: ObjectID(req.params.id) },
+        { $set: { name: req.body.name } },
+        (err, result) => {
+            if (err) {
+                console.log("123")
+                console.log(err);
+                return res.sendStatus(500)
+            }
+            res.sendStatus(200)
+        }
+    )
 })
-app.delete('/artists/:id', (req, res)=>{
-  db.collection('artists').deleteOne(
-      {_id: ObjectID(req.params.id) },(err, result)=>{
-          if (err) {
-              console.log(err);
-              return res.sendStatus(500);
-          }
-          res.sendStatus(200);
-      }
-  )
+app.delete('/artists/:id', (req, res) => {
+    db.collection('artists').deleteOne(
+        { _id: ObjectID(req.params.id) }, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
 })
-MongoClient.connect('mongodb://localhost:27017', (err, database)=>{
-    if(err) {
-       return console.log(err);
+MongoClient.connect('mongodb://localhost:27017', (err, database) => {
+    if (err) {
+        return console.log(err);
     }
     db = database.db('mybase');
-app.listen(5001, ()=>{
-    console.log("Api app started sucsesfully")
-}) 
+    app.listen(5001, () => {
+        console.log("Api app started sucsesfully")
+    })
 })
